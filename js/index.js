02ea9d95fb4676fd804c46425f6ae1acfd758f1b
aian0517 <qaz0906461562@gmail.com>
2023-08-24 16:53:22 +0800

    1

--- js/index.js
+++ js/index.js
@@ -53,6 +53,8 @@ var body3_swp = new Swiper(".body3-swp",{
 })
 
 
+
+
 // nav
 // var y = $(window).scrollTop()
 // $(window).scroll(()=>{
@@ -183,54 +185,193 @@ Highcharts.chart('mychart',{
 })
 
 
-// sun
+sun
 var sun = 1
 $('.sun').click(()=>{
    if(sun === 1){
-    $('body').css('--bg--color','#f78614')
-    $('body').css('--bg--color2','#2894FF')
+    $('body').css('--bg--color','#ec953d')
+    $('body').css('--bg--color2','#599fe6')
     $('body').css('--bg--sun','#5d5d5d')
     $('body').css('--color--sun','#fff')
     $('.sun').attr('src','./image/nav-icon3-moon.png')
-    $('.footer-runbg').css('background-image','url(../image/footer-bg2.png)')
     sun = 0
   }else if(sun === 0){
-    $('body').css('--bg--color','#2894FF')
-    $('body').css('--bg--color2','#f78614')
+    $('body').css('--bg--color','#599fe6')
+    $('body').css('--bg--color2','#ec953d')
     $('body').css('--bg--sun','#fff')
     $('body').css('--color--sun','#000')
     $('.sun').attr('src','./image/nav-icon3.png')
-    $('.footer-runbg').css('background-image','url(../image/footer-bg.png)')
     sun = 1
   }
 })
 
 
-// $('.color-btn').click(()=>{
-//   var form_color = $('.form-color').val()
-//   var form_color2 =$('.form-color2').val()
-//   $('body').css('--bg--color',form_color)
-//   $('body').css('--bg--color2',form_color2)
-// })
+$('.color-btn').click(()=>{
+  var form_color = $('.form-color').val()
+  var form_color2 =$('.form-color2').val()
+  $('body').css('--bg--color',form_color)
+  $('body').css('--bg--color2',form_color2)
+})
 
+// shop
 
+var shop_swiper = new Swiper('.shop-swiper',{
+  slidesPerView:3,
+  spaceBetween:10,
+  garbCursor:true,
+  breakpoints:{
+    0:{
+      slidesPerView:1,
+    },
+    1080:{
+      slidesPerView:3,
+    }
+  },
+  pagination:{
+    el:'.shop-pagination',
+  },
+})
+$.each(shop_data,function(index,value){
+  $('.shop-wrapper').append(`
+    <div class="swiper-slide">
+      <div class="card p-2">
+        <img src="${value.img}" alt="" class="card-img-top shop-img">
+        <div class="card-body df justify-content-around flex-column">
+            <div class="card-title shop-title">
+              ${value.title}
+            </div>
+            <div class="card-text shop-text">
+                <p class="mb-2">${value.text}</p>
+                <p class="color2 text-center fw-bold fs-5" >NT$${value.price_text}/一天</p>
+            </div>
+            <div class="shop-control">
+                <button class="shop-btn-reduce" onclick= add(-1,${index})>-</button>
+                <input type="text" class="shop-input" value="${value.amount}">
+                <button class="shop-btn-add" onclick= add(1,${index})>+</button>
+            </div>
+        </div>
+      </div>
+    </div>
+    `)
+})
+var shop_price = 0
+function add(n,index){
+  shop_data[index].amount = Math.max(0,shop_data[index].amount + n)
+  $('.shop-input').eq(index).val(shop_data[index].amount)
+  ccc()
+}
+$('.shop-input').on('input',function(){
+  var shop_input_index = $(this).index('.shop-input')
+  var shop_input_val = $(this).val()
+  shop_data[shop_input_index].amount = shop_input_val
+  ccc()
+})
+$('.shop-input').on('blur',function(){
+  var shop_input_index = $(this).index('.shop-input')
+  var shop_input_val = $(this).val()
+  if($(this).val()===''){
+    $(this).val(0)
+    ccc()
+  }
+})
+function ccc(){
+  shop_price = 0
+  $('.shop-cart-content').html('')
+  $.each(shop_data,function(ind,item){
+    if(item.amount != 0){
+      shop_price += item.price * item.amount
+      $('.shop-cart-content').prepend(`
+      <div class="shop-cart-data active">
+        <h5 class="color fw-bold">${item.title}</h5>
+        <div class="df justify-content-between">
+            <p>${item.amount}堂</p>
+            <p>NT$${item.price_text}/一天</p>
+        </div>
+      </div>
+      `)
+    }
+  })
+  $('.shop-total-price').text(shop_price)
+}
+$('.shop-submit').click(()=>{
+  if($('.shop-total-price').text() != 0){
+    $('#shop_submit').modal('show')
+  }
+})
+$('.shop-submit-submit').click(()=>{
+  $('.shop-cart-content').html('')
+  $('.shop-total-price').text(0)
+  $('.shop-submit-input').val('')
+  shop_data.map(x => x.amount = 0)
+  $('.shop-input').val(0)
+})
 
 // message
+  var mes = new Swiper(".mes",{
+  loop:true,
+  autoplay:{
+    delay:3000,
+    disableOnInteraction:false,
+  },
+  grabCursor: true,
+  navigation:{
+    nextEl:'.swiper-button-next',
+    prevEl:'.swiper-button-prev',
+  }
+  })
+
+var message_name = []
+var message_email = []
+var message_mess = []
+
 function message(){
   window.event.preventDefault()
-  $('.message').append(`
-  <div class="message-1 mb-2">
-      <div class="df justify-content-between mb-2">
-          <h5 class="mb-0">${$('.message-name').val()}</h5>
-          <p class="fs-6 mb-0">${$('.message-email').val()}</p>
+  message_name.push($('.message-name').val())
+  message_email.push($('.message-email').val())
+  message_mess.push($('.message-mess').val())
+  localStorage.setItem('message-name',JSON.stringify(message_name))
+  localStorage.setItem('message-email',JSON.stringify(message_email))
+  localStorage.setItem('message-mess',JSON.stringify(message_mess))
+
+  mes.appendSlide(`
+  <div class="swiper-slide df jcc aic">
+      <div class="mes-content">
+          <h2 class="mb-2 fw-bold">${$('.message-name').val()}</h2>
+          <p class="fs-6 mb-1 text-end fw-bold">${$('.message-email').val()}</p>
+          <div class="message-wire"></div>
+          <p class="fs-5 mes-text">${$('.message-mess').val()}
+          </p>
       </div>
-      <p class="message-1-wire fs-4">
-          ${$('.message-input').val()}
-      </p>
   </div>
   `)
   $('.message-box input').val('')
+  var newIndex = mes.slides.length - 1;
+  mes.slideTo(newIndex, 0);
+  mes.autoplay.start()
+  
 }
+$(window).on('load',()=>{
+  const local_name = JSON.parse(localStorage.getItem('message-name'))
+  const local_email = JSON.parse(localStorage.getItem('message-email'))
+  const local_mess =JSON.parse(localStorage.getItem('message-mess'))
+  $.each(local_name,function(index,value){
+    mes.appendSlide(`
+    <div class="swiper-slide df jcc aic">
+        <div class="mes-content">
+            <h2 class="mb-2 fw-bold">${value}</h2>
+            <p class="fs-6 mb-1 text-end fw-bold">${local_email[index]}</p>
+            <div class="message-wire"></div>
+            <p class="fs-5 mes-text">${local_mess[index]}
+            </p>
+        </div>
+    </div>
+    `)
+  })
+  message_name.push(local_name)
+  message_email.push(local_email)
+  message_mess.push(local_mess)
+})
+
 
 
 // robot
@@ -275,3 +416,4 @@ $('.robot-a').click(function(){
   $('.robot-input').val(text)
   $('.robot-submit').click()
 })
+
